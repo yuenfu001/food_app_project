0e03942e63a77f5c980cc65891ca88ba661d86ed
liu yuenfu <liuyuenfu@gmail.com>
2022-02-13 15:44:08 +0100

    added requirements and README

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# INSTRUCTION
+
+## Do install the following using poetry environment by using *poetry add* command
+
+1. django==4.0 *poetry add django==4.0*
+2. python-decouple==3.6 *poetry add python-decouple==3.6*
+3. add a .env file to your project
--- food_app_project/settings.py
+++ food_app_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!xvo5$iug3%k=t45-8$*7+zqcofu3k1l(1@0ribx0$v-i&c)xn'
-
+SECRET_KEY = config("KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("BUG")
 
 ALLOWED_HOSTS = []
 
